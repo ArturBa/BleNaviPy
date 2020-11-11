6e506ad8a61c468b25abfe3e0be648864667068f
Artur Bauer <bauerart1@gmail.com>
2020-11-11 14:54:10 +0100

    Add future annotations support on location

--- bleNaviPy/beacon/location.py
+++ bleNaviPy/beacon/location.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import math
 
 
@@ -11,25 +13,33 @@ class Location:
         self.x = x
         self.y = y
 
-    def distance(self, l) -> float:
-        x_distance = math.pow(self.x - l.x, 2)
-        y_distance = math.pow(self.y - l.y, 2)
+    def distance(self, location: Location) -> float:
+        """Calculate distance between 2 locations
+
+        Args:
+            location (Location):
+
+        Returns:
+            float: distance between locations
+        """
+        x_distance = math.pow(self.x - location.x, 2)
+        y_distance = math.pow(self.y - location.y, 2)
         distance = math.sqrt(x_distance + y_distance)
         return distance
 
-    def __eq__(self, loc):
-        return self.x == loc.x and self.y == loc.y
+    def __eq__(self, location: Location):
+        return self.x == location.x and self.y == location.y
 
     def __str__(self):
         return f"Location x: {self.x}\ty: {self.y}"
 
-    def __add__(self, loc):
-        self.x += loc.x
-        self.y += loc.y
+    def __add__(self, location: Location):
+        self.x += location.x
+        self.y += location.y
         return self
 
-    def __sub__(self, loc):
-        print(self, loc)
-        self.x -= loc.x
-        self.y -= loc.y
+    def __sub__(self, location: Location):
+        print(self, location)
+        self.x -= location.x
+        self.y -= location.y
         return self
