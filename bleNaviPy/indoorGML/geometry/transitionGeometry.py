bc1653a71812576a680cfac3a8800f421e61ff4b
Artur Bauer <bauerart1@gmail.com>
2021-03-20 11:53:33 +0100

    Add: Distance calculation for transition

--- bleNaviPy/indoorGML/geometry/transitionGeometry.py
+++ bleNaviPy/indoorGML/geometry/transitionGeometry.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import math
+
+import numpy as np
+from numpy.linalg import norm
+
 from bleNaviPy.indoorGML.geometry.pointGeometry import Point
 
 
@@ -14,7 +19,16 @@ class TransitionGeometry:
         return f"Transition: " + s
 
     def getDistance(self, point: Point) -> double:
-        return 0.3
+
+        p3 = np.asarray([point.x, point.y])
+        distance = float("inf")
+        for i in range(1, len(self.points)):
+            p1 = np.asarray([self.points[i - 1].x, self.points[i - 1].y])
+            p2 = np.asarray([self.points[i].x, self.points[i].y])
+            d = norm(np.cross(p2 - p1, p1 - p3)) / norm(p2 - p1)
+            if d < distance:
+                distance = d
+        return round(distance, 4)
 
     def getTheClosestPoint(self, point: Point) -> Point:
         return Point(0, 0.5)
--- tests/indoorGML/geometry/transitionGeometry_test.py
+++ tests/indoorGML/geometry/transitionGeometry_test.py
@@ -5,21 +5,29 @@ from bleNaviPy.indoorGML.geometry.transitionGeometry import TransitionGeometry
 
 
 class LocationTest(unittest.TestCase):
-    transitionPoints = [Point(0, 0), Point(0, 1)]
+    transitionPoints = [Point(0, 0), Point(1, 0), Point(1, -1)]
     transitionGeometry = TransitionGeometry(transitionPoints)
 
     def testInit(self):
         self.assertEqual(self.transitionPoints, self.transitionGeometry.points)
 
     def testStringify(self):
+        transitionPoints = [Point(0, 0), Point(0, 1)]
+        transitionGeometry = TransitionGeometry(transitionPoints)
         self.assertEqual(
-            f"Transition: [{self.transitionPoints[0].x}, {self.transitionPoints[0].y}][{self.transitionPoints[1].x}, {self.transitionPoints[1].y}]",
-            self.transitionGeometry.__str__(),
+            f"Transition: [{transitionPoints[0].x}, {transitionPoints[0].y}][{transitionPoints[1].x}, {transitionPoints[1].y}]",
+            transitionGeometry.__str__(),
         )
 
     def testDistance(self):
-        testPoint = Point(0.3, 0.5)
+        testPoint = Point(0.3, 0.3)
         self.assertEqual(0.3, self.transitionGeometry.getDistance(testPoint))
+
+        testPoint = Point(0.3, 0.5)
+        self.assertEqual(0.5, self.transitionGeometry.getDistance(testPoint))
+
+        testPoint = Point(0.9, -0.9)
+        self.assertEqual(0.1, self.transitionGeometry.getDistance(testPoint))
 
     def testClosestPoint(self):
         testPoint = Point(0.3, 0.5)
